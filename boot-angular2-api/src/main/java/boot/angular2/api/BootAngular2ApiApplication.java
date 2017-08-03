bbf46a3560d58b821ca283ba4dd6048ecb816b4a
sabin <nibaserc@gmail.com>
2017-08-03 14:02:02 -0500

    Project initial SetUp

---
+++ boot-angular2-api/src/main/java/boot/angular2/api/BootAngular2ApiApplication.java
@@ -0,0 +1,12 @@
+package boot.angular2.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootAngular2ApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootAngular2ApiApplication.class, args);
+	}
+}
---
+++ boot-angular2-api/src/test/java/boot/angular2/api/BootAngular2ApiApplicationTests.java
@@ -0,0 +1,16 @@
+package boot.angular2.api;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BootAngular2ApiApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
