7035e52b7fb9a58d3f06deacdd722f1a68fe3248
sabin <nibaserc@gmail.com>
2017-08-03 15:16:49 -0500

    Mock data added

---
+++ boot-angular2-api/src/main/java/boot/angular2/api/mock/data/DataSetUp.java
@@ -0,0 +1,36 @@
+package boot.angular2.api.mock.data;
+
+import boot.angular2.api.domain.Article;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+
+import java.io.File;
+import java.util.List;
+
+/**
+ * Created by sabin on 8/3/2017.
+ */
+@Configuration
+public class DataSetUp implements CommandLineRunner {
+
+    @Value("${url}")
+    private String url;
+
+    @Override
+    public void run(String... strings) throws Exception {
+        setUpData(url);
+    }
+
+    public void setUpData(String url) throws Exception {
+        Resource rsrc = new ClassPathResource(url);
+        String path = rsrc.getFile().getAbsolutePath();
+        ObjectMapper mapper = new ObjectMapper();
+        List<Article> articles = mapper.readValue(new File(path), mapper.getTypeFactory().constructCollectionType(List.class, Article.class));
+        System.out.print(articles.size());
+
+    }
+}
